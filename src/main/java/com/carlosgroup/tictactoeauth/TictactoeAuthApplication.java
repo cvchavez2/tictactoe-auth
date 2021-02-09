6ccc73dfa0cd3485c2cf9eb7bfcc0a6178d83723
cvchavez2 <cvchavez92@gmail.com>
2021-02-09 11:51:20 -0700

    spring proj

---
+++ src/main/java/com/carlosgroup/tictactoeauth/TictactoeAuthApplication.java
@@ -0,0 +1,13 @@
+package com.carlosgroup.tictactoeauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TictactoeAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TictactoeAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlosgroup/tictactoeauth/TictactoeAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosgroup.tictactoeauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TictactoeAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
